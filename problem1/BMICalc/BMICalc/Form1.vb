9a2db784523dba9663df000984c8e52fd8dba890
idsc3101 <idsc3101@gmail.com>
2013-09-11 15:22:46 -0500

    I solved it

--- problem1/BMICalc/BMICalc/Form1.vb
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -3,6 +3,17 @@
 Public Class Form1
 
     Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
+        Dim weight As String
+        Dim height As String
+        Dim bmi As Double
+        Dim w As Double
+        Dim h As Double
+        weight = txtWeight.Text
+        w = CDbl(weight)
+        height = txtHeight.Text
+        h = CDbl(height)
+        bmi = (w * 703) / (h ^ 2)
+        TextBox1.Text = CStr(bmi)
 
     End Sub
 End Class
